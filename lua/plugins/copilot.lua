d965af1b4af9900277ab1148f846b72b033f0f3f
Daniel Kipp <daniel.kipp@gmail.com>
2025-09-09 11:07:47 +0200

    cleaning up; adding new php config for testing

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -40,12 +40,7 @@ return {
 
     -- Update LazyVim's cmp.actions to handle Copilot suggestions
     LazyVim.cmp.actions.ai_accept = function()
-      local copilot = require("copilot.suggestion")
-      if copilot.is_visible() then
-        LazyVim.create_undo() -- Create undo point before accepting suggestion
-        copilot.accept() -- Accept the current Copilot suggestion
-        return true
-      end
+      return false
     end
   end,
 }

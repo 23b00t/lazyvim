410efc8da3ebddd759e27d701a3999f4e24e0735
23b00t <daniel.kipp@gmail.com>
2025-09-18 09:20:28 +0200

    disable line-length linting in md

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,1 +1,12 @@
-return {}
+local HOME = os.getenv("HOME")
+return {
+  "mfussenegger/nvim-lint",
+  optional = true,
+  opts = {
+    linters = {
+      ["markdownlint-cli2"] = {
+        args = { "--config", HOME .. "/.markdownlint-cli2.yaml", "--" },
+      },
+    },
+  },
+}

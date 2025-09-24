3cf04aff5b39a602532715fe8d2bd11ebab754d3
Daniel Kipp <daniel.kipp@gmail.com>
2025-09-24 23:52:13 +0200

    change vsmulti mappings; remove fzf stuff

--- lua/plugins/visual-multi.lua
+++ lua/plugins/visual-multi.lua
@@ -3,8 +3,8 @@ return {
 		"mg979/vim-visual-multi",
 		branch = "master",
 		keys = {
-			{ "<C-j>", "<Plug>(VM-Add-Cursor-Down)", desc = "Visual Multi: Add Cursor Down", mode = { "n" } },
-			{ "<C-k>", "<Plug>(VM-Add-Cursor-Up)", desc = "Visual Multi: Add Cursor Up", mode = { "n" } },
+      { "<A-d>", "<Plug>(VM-Add-Cursor-Down)", desc = "Visual Multi: Add Cursor Down", mode = { "n" } },
+			{ "<A-e>", "<Plug>(VM-Add-Cursor-Up)", desc = "Visual Multi: Add Cursor Up", mode = { "n" } },
 		},
 		init = function()
 			vim.g.VM_theme = "purblegray"

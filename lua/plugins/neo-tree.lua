3425bfe374d978f2e5e7abbdce193f3cd93f5a03
23b00t <daniel.kipp@gmail.com>
2025-09-30 13:26:12 +0200

    config neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      width = 60,
+    },
+  },
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ action = "focus", dir = LazyVim.root() })
+      end,
+      desc = "Explorer NeoTree (Root Dir)",
+    },
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute({ action = "focus", dir = vim.uv.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+  },
+}

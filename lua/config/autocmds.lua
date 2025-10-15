92daecec70e4c37d79d1734a24b193741f589893
23b00t <daniel.kipp@gmail.com>
2025-10-15 12:37:44 +0200

    dissable treesitter php fix

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -28,21 +28,21 @@ end
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 -- php fix
-local group = vim.api.nvim_create_augroup("PHPIndentFix", { clear = true })
-
-vim.api.nvim_create_autocmd("FileType", {
-	group = group,
-	pattern = "php",
-	callback = function()
-		-- Verzögerung, um sicherzustellen, dass unsere Einstellungen nach
-		-- allen anderen Initialisierungen angewendet werden
-		vim.defer_fn(function()
-			-- Nur das TreeSitter-Indenting deaktivieren, nicht die ganze Funktionalität
-			vim.bo.indentexpr = "" -- Entferne das TreeSitter Indent-Expression
-			vim.bo.autoindent = true
-			vim.bo.cindent = false
-			vim.bo.smartindent = true
-			vim.bo.shiftwidth = 4
-		end, 10)
-	end,
-})
+-- local group = vim.api.nvim_create_augroup("PHPIndentFix", { clear = true })
+--
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	group = group,
+-- 	pattern = "php",
+-- 	callback = function()
+-- 		-- Verzögerung, um sicherzustellen, dass unsere Einstellungen nach
+-- 		-- allen anderen Initialisierungen angewendet werden
+-- 		vim.defer_fn(function()
+-- 			-- Nur das TreeSitter-Indenting deaktivieren, nicht die ganze Funktionalität
+-- 			vim.bo.indentexpr = "" -- Entferne das TreeSitter Indent-Expression
+-- 			vim.bo.autoindent = true
+-- 			vim.bo.cindent = false
+-- 			vim.bo.smartindent = true
+-- 			vim.bo.shiftwidth = 4
+-- 		end, 10)
+-- 	end,
+-- })

6c68747a16785ce0e87f5e9caf115f6c690df0ad
Daniel Kipp <daniel.kipp@gmail.com>
2025-12-30 21:15:06 +0100

    refined godot config

--- lua/plugins/godot.lua
+++ lua/plugins/godot.lua
@@ -1,10 +1,5 @@
 return {
 	{
-		"habamax/vim-godot",
-	},
-	-- { "skywind3000/asyncrun.vim" },
-	{ "teatek/gdscript-extended-lsp.nvim" },
-	{
 		"mfussenegger/nvim-dap",
 		optional = true,
 		opts = function()
@@ -20,20 +15,22 @@ return {
 					request = "launch",
 					name = "Debug Godot Scene",
 					project = "${workspaceFolder}",
-					-- launch_scene = true,
+					launch_scene = true,
 				},
 			}
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				gdscript = {
-					filetypes = { "gd", "gdscript" },
-				},
-			},
-		},
+		config = function()
+			require("lspconfig").gdscript.setup({
+				filetypes = { "gd", "gdscript" },
+				root_dir = function(fname)
+					return require("lspconfig.util").root_pattern("project.godot")(fname)
+						or require("lspconfig.util").root_pattern(".git")(fname)
+				end,
+			})
+		end,
 	},
 	{
 		"stevearc/conform.nvim",
@@ -59,6 +56,41 @@ return {
 					vim.opt_local.expandtab = true
 				end,
 			})
+		end,
+	},
+	{
+		"junegunn/fzf.vim",
+		dependencies = { "junegunn/fzf" },
+		lazy = false,
+	},
+	{
+		"habamax/vim-godot",
+		dependencies = { "junegunn/fzf.vim" },
+	},
+	{ "skywind3000/asyncrun.vim" },
+	-- {
+	-- 	"teatek/gdscript-extended-lsp.nvim",
+	-- 	config = function()
+	-- 		require("gdscript-extended-lsp").setup({
+	-- 			-- doc_file_extension = ".txt", -- Documentation file extension (can allow a better search in buffers list with telescope)
+	-- 			view_type = "floating", -- Options : "current", "split", "vsplit", "tab", "floating"
+	-- 			-- split_side = false, -- (For split and vsplit only) Open on the right or top on false and on the left or bottom on true
+	-- 			-- keymaps = {
+	-- 			-- 	declaration = "gd", -- Keymap to go to definition
+	-- 			-- 	close = { "q" }, -- Keymap for closing the documentation
+	-- 			-- },
+	-- 			-- floating_win_size = 0.8, -- Floating window size
+	-- 			-- picker = "telescope", -- Options : "telescope", "snacks", "fzf-lua"
+	-- 		})
+	-- 	end,
+	-- },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			if opts.ensure_installed == nil then
+				opts.ensure_installed = {}
+			end
+			vim.list_extend(opts.ensure_installed, { "gdscript" })
 		end,
 	},
 }

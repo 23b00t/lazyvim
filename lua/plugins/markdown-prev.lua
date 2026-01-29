b6afcd41dba7371abaf3a861582a4e7c1555c589
23b00t <daniel.kipp@gmail.com>
2026-01-29 12:20:59 +0100

    update; fix markdown prev

--- lua/plugins/markdown-prev.lua
+++ lua/plugins/markdown-prev.lua
@@ -1,11 +1,11 @@
 return {
   "iamcco/markdown-preview.nvim",
-  opts = function()
-    vim.cmd([[
-      function! OpenPreviewInKittyAwrit(url)
-        call system('kitty @ launch awrit ' . shellescape(a:url))
-      endfunction
-      let g:mkdp_browserfunc = 'OpenPreviewInKittyAwrit'
-    ]])
-  end,
+  -- opts = function()
+  --   vim.cmd([[
+  --     function! OpenPreviewInKittyAwrit(url)
+  --       call system('kitty @ launch awrit ' . shellescape(a:url))
+  --     endfunction
+  --     let g:mkdp_browserfunc = 'OpenPreviewInKittyAwrit'
+  --   ]])
+  -- end,
 }

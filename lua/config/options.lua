1a6a9bb9dafc0328581f3febef6bf05a82bfde35
Daniel Kipp <daniel.kipp@gmail.com>
2026-04-03 15:11:53 +0200

    Imporved copying to host clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -45,38 +45,16 @@ local function osc52_copy(text)
 	vim.api.nvim_chan_send(vim.v.stderr, osc)
 end
 
--- Helper to get the current visual selection
-local function get_visual_selection()
-	local s_start = vim.fn.getpos("'<")
-	local s_end = vim.fn.getpos("'>")
-	local n_lines = math.abs(s_end[2] - s_start[2]) + 1
-	local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
-
-	if #lines == 0 then
-		return ""
-	end
-
-	lines[1] = string.sub(lines[1], s_start[3], -1)
-	if n_lines == 1 then
-		lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
-	else
-		lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
-	end
-	return table.concat(lines, "\n")
-end
-
--- Mapping for visual selection
-vim.keymap.set("v", "<localleader>y", function()
-	-- Exit visual mode to update marks '< and '>
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "x", false)
-	local text = get_visual_selection()
-	osc52_copy(text)
-	vim.notify("Copied selection to host clipboard (OSC52)", vim.log.levels.INFO)
-end, { desc = "Copy selection to clipboard (OSC52)" })
-
--- Mapping for current line in normal mode
-vim.keymap.set("n", "<localleader>y", function()
-	local text = vim.api.nvim_get_current_line()
-	osc52_copy(text)
-	vim.notify("Copied line to host clipboard (OSC52)", vim.log.levels.INFO)
-end, { desc = "Copy line to clipboard (OSC52)" })
+-- Autocmd to copy yanked text to host clipboard using OSC52
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		local event = vim.v.event
+		if event.operator == "y" and event.regname == "" then -- nur Standardregister
+			local yanked = vim.fn.getreg('"')
+			if yanked and #yanked > 0 then
+				osc52_copy(yanked)
+			end
+		end
+	end,
+	desc = "Copy yanked text to host clipboard (OSC52)",
+})

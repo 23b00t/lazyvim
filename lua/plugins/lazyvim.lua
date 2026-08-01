6ccf4dcf2a848409ff9111679234690f14fedb51
Daniel Kipp <daniel.kipp@gmail.com>
2026-08-01 17:06:24 +0200

    Unpin LazyVim and update

--- lua/plugins/lazyvim.lua
+++ lua/plugins/lazyvim.lua
@@ -1,5 +1,5 @@
 return {
   -- wait for > 15.15.0
-  "LazyVim/LazyVim",
-  commit = "7c1301b89563198d1b2a295e673f6a7205adc076",
+  -- "LazyVim/LazyVim",
+  -- commit = "7c1301b89563198d1b2a295e673f6a7205adc076",
 }

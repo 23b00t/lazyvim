4ac072f3a90c5ab939579cc47508ad092b4c76fb
Daniel Kipp <daniel.kipp@gmail.com>
2026-08-02 20:27:10 +0200

    nix config

--- lua/plugins/nix.lua
+++ lua/plugins/nix.lua
@@ -1,8 +1,28 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      nixd = {},
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        nil_ls = false,
+        nixd = {
+          settings = {
+            nixd = {
+              formatting = {
+                command = { "nixfmt" },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        nix = { "statix", "deadnix" },
+      },
     },
   },
 }
